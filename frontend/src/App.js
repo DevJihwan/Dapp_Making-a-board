d25f73b975dc9347deb7e7952194bb818c456feb
DevJihwan <jh552016@naver.com>
2022-08-17 08:56:17 +0900

    commit on main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,58 @@
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
 import getWeb3 from "./getWeb3.js";
 
-function App() {
+class App extends Component {
 
-  function getMetaMaskAddress() {
-    let _web3;
+  constructor(props) {
+    super(props);
+    this.state = {
+      userAccount: null,
+      userToken: null,
+      web3:null
+    };
+  }
+
+  componentDidMount() {
+    console.log("#########START componetDidMount########");
+    //getWeb3.js에서 결과를 리턴 받는다. 
+    getWeb3.then(results => {
+        console.log("#########Step01.componetDidMount : results########"+results);
+        //받은 결과를 let web3에 저장
+        this.setState({
+          web3: results
+      });
+   }).catch(() => {
+       console.log("Error finding web3.");
+   });
+}
+
+  getMetaMaskAddress = async() => {
     let _metaAccount;
 
-    getWeb3.then(results => {
-      console.log("#########Step01.getMetaMaskAddress : results########"+results);
-      //받은 결과를 let web3에 저장
-      // let _web3 = results;
-      _web3 = results;
-      _metaAccount = _web3.eth.requestAccounts();
-      console.log("#########Step02.getMetaMaskAddress : _metaAccount########"+_metaAccount);
-  }).catch(() => {
-    console.log("Error finding web3.");
-  });
-  console.log("#########Step03.getMetaMaskAddress : _metaAccount########"+_metaAccount);
-  
+    _metaAccount = this.state.web3.currentProvider.selectedAddress;
+    console.log("#########Step02.getMetaMaskAddress : _metaAccount########"+_metaAccount);
   }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <button 
-          className="App-link"
-          onclick = {getMetaMaskAddress()}
-        >
-          Get MetaMask Address
-        </button>
-      </header>
-    </div>
-  );
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/* <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p> */}
+          <button 
+            className="App-link"
+            onClick = {() => this.getMetaMaskAddress()}
+          >
+            Get MetaMask Address
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;

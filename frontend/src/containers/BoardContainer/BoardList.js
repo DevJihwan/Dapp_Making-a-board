0b4822b37af82f1b4f09b0ebb14f0aa6315cda2c
DevJihwan <jh552016@naver.com>
2022-09-28 18:08:38 +0900

    commit on main

--- frontend/src/containers/BoardContainer/BoardList.js
+++ frontend/src/containers/BoardContainer/BoardList.js
@@ -5,6 +5,13 @@ import axios from "axios";
 
 class BoardList extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+          item:[]
+        };
+    }
+
     //Step01. 화면을 그릴때 로그인 페이지에서 보내준 사용자 id를 받아서 상태값에 저장 
     componentDidMount() {
         const getList = axios.get(
@@ -13,33 +20,45 @@ class BoardList extends Component {
             //Parameter Data
             ).then((response) => {
                 console.log("########## getListComplete ########"+response);
+                
+                this.makeBoardList(response);
 
-                /*
-                let arrayLength = response.data._embedded.boards.length;
-                let _agree_cnt, _title, _content, _user_id;
-
-                for (let i=0; i<arrayLength; i++){
-                    _agree_cnt = response.data._embedded.boards[i].agree_cnt;
-                    _title = response.data._embedded.boards[i].title;
-                    _content = response.data._embedded.boards[i].article;
-                    _user_id = response.data._embedded.boards[i].writer_userid;    
-                }
-                */
-
-                const item = (Object.values(response.data)).map((item) => (
-                    <BoardListRow key={item.}
-                ));
-            })
+                })
         
         }
+
+    makeBoardList(res) {
+
+        let arrayLength = res.data._embedded.boards.length;
+        let _agree_cnt, _title, _content, _user_id;
+
+        for (let i=0; i<arrayLength; i++){
+            _agree_cnt = res.data._embedded.boards[i].agree_cnt;
+            _title = res.data._embedded.boards[i].title;
+            _content = res.data._embedded.boards[i].article;
+            _user_id = res.data._embedded.boards[i].writer_userid;    
+
+        const _item  = (
+            <BoardListRow key={i}>
+                <BoardListColumn>{_agree_cnt}</BoardListColumn>
+                <BoardListColumn>{_title}</BoardListColumn>
+                <BoardListColumn>{_content}</BoardListColumn>
+                <BoardListColumn>{_user_id}</BoardListColumn>
+            </BoardListRow>
+        );
+
+        this.setState({
+            item:this.state.item.concat(_item)
+        })
+        }
+
+    }
 
 
     render() {
         return (
             <BoardContent title="게시판목록">
-                <BoardListComponent headerName={['글번호', '제목', '등록일', '작성자']}>
-                    
-                </BoardListComponent>
+                {this.state.item}
             </BoardContent>
         );
     }

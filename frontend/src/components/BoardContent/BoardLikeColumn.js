598b44ab4ab0c1eb30b9ca8fc72b2a023f929545
DevJihwan <jh552016@naver.com>
2022-10-06 13:47:00 +0900

    feat:Board Like 기능 추가 (disLike 기능 x)

--- frontend/src/components/BoardContent/BoardLikeColumn.js
+++ frontend/src/components/BoardContent/BoardLikeColumn.js
@@ -5,26 +5,23 @@ const BoardLikeColumn = ({ children }) => {
   // console.log(" TEST1 #$%#$%#$#% "+props.value);
   // console.log(" TEST2 #$%#$%#$#% "+this.props);
   console.log(" TES3 #$%#$%#$#% "+children);
-  console.log(" TES4 #$%#$%#$#% "+parseInt(children));
-  console.log(" TES5 #$%#$%#$#% "+Number(children));
+  // console.log(" TES4 #$%#$%#$#% "+parseInt(children));
+  // console.log(" TES5 #$%#$%#$#% "+Number(children));
 
-  let _value = children;
-
-  console.log(" TES6 #$%#$%#$#% "+_value);
-
-  _value = _value.replace(".","")
-  console.log(" TES7 #$%#$%#$#% "+_value);
-
-  const [value, setValue] = useState(parseInt(children));
+  const [value, setValue] = useState(0);
 
   function clickLike(){
-    setValue(value + parseInt(1));
+    setValue(value + 1);
     console.log(value+"%%%%");
   }
 
+  const data = parseInt(value) + Number(children[1]);
+
+  console.log(" TES5 #$%#$%#$#% "+data);
+
   return (
     <td className="common-table-column" onClick={() => clickLike()}>
-      👍 {value}
+      👍 {data}
     </td>
   )
 }
